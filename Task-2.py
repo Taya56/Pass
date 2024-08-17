7972e2e2d36d5097d0acb265d8a6b78e2eeebb76
Taya56 <krasnyanskay78@gmail.com>
2024-08-17 20:16:44 +0300

    17,08,2024 20:16

---
+++ Task-2.py
@@ -0,0 +1,5 @@
+NumberOfCompletedDZ= 12
+NumberOfHoursSpent= 1.5
+CourseName='Python'
+TimePerTask = NumberOfHoursSpent/NumberOfCompletedDZ
+print(CourseName,', всего задач:',NumberOfCompletedDZ,', затрачено часов:',NumberOfHoursSpent,', среднее время выполнения',TimePerTask, 'часа')
