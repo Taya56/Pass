e9478d2524385c453f254be770039c6c0f1bf2d9
Taya56 <krasnyanskay78@gmail.com>
2024-08-17 21:48:27 +0300

    Отладка

--- Task-2.py
+++ Task-2.py
@@ -2,4 +2,4 @@ NumberOfCompletedDZ= 12
 NumberOfHoursSpent= 1.5
 CourseName='Python'
 TimePerTask = NumberOfHoursSpent/NumberOfCompletedDZ
-print(CourseName,', всего задач:',NumberOfCompletedDZ,', затрачено часов:',NumberOfHoursSpent,', среднее время выполнения',TimePerTask, 'часа')
+print(f"{CourseName}, всего задач: {NumberOfCompletedDZ}, затрачено часов: {NumberOfHoursSpent}, среднее время выполнения: {TimePerTask} часа")
