56cd03c5949ecf3f056f95423628e7fe1dbfabb3
Taya56 <94736089+Taya56@users.noreply.github.com>
2024-08-28 14:54:26 +0300

    Rename Task-2.py to module_1_2.py

--- Task-2.py
+++
@@ -1,5 +0,0 @@
-NumberOfCompletedDZ= 12
-NumberOfHoursSpent= 1.5
-CourseName='Python'
-TimePerTask = NumberOfHoursSpent/NumberOfCompletedDZ
-print(f"{CourseName}, всего задач: {NumberOfCompletedDZ}, затрачено часов: {NumberOfHoursSpent}, среднее время выполнения: {TimePerTask} часа")
