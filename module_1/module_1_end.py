b5bd9f91b15bbbd7306fb8ca441dfc7a3ba63ede
Taya56 <krasnyanskay78@gmail.com>
2024-09-05 18:26:47 +0300

    Работа над ошибками

--- module_1/module_1_end.py
+++ module_1/module_1_end.py
@@ -1,42 +1,19 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
+#Работа над ошибками
+
+#Преподаватель сказал:" Вам осталось отсортировать методом sorted весь ваш список студентов."
+    #Узнала, как работает sorted и sort.
+#Еще 1-а ошибка: У меня, за приделами цикла for, выводится только часть списка - что является ошибкой, как я позже узнала.
+    #Что бы это исправить выношу average_score, за пределы цикла.
+#Ошибка 2:  строка <students=list(students)>, заменяется на <students=sorted(students)>
+
+average_score={}
+      
 for i in range(len(grades)):
-    lst=sum(grades[i])/len(grades[i])
-
-    students=list(students)
-    
-    average_score={}
-        
+    lst=sum(grades[i])/len(grades)     
+    students=sorted(students)
     average_score[students[i]] = lst
-    print(average_score)
+print(average_score)
 
-# Это мой мозговой процесс.
-
-# Мне надо узнать среднию оценку у значений в списке grades. Для того чтобы ускорить процесс, делаю это через for.
-# Однако возникает проблема в бесконечном повторение цикла. Делаю ограничение до длины grades.
-# Возникла проблема в том что у students и grades, разный тип данных, т.к. они не совместимы. Потому меняю тип данных у students, на список.
-# Далее создаю переменую, словарь, в котором будут храниться все данные.
-
-# Мне было трудно представить как можно было совместить список и множество в словарь потому я поискала как это впринципе делаеться.
-# Ссылка на материал которым я воспользывалась https://translated.turbopages.org/proxy_u/en-ru.ru.1691d5b5-66d4302a-4044251c-74722d776562/https/www.geeksforgeeks.org/how-to-initialize-a-dictionary-in-python-using-for-loop
-   
-    
-
-
-
-
-
-    
-
-
-
-
-    
- 
-    
-
-
-
-
-    
