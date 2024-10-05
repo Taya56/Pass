1ad16aa4f1c4d91f615930fce38783b6d5832991
Taya56 <krasnyanskay78@gmail.com>
2024-10-05 17:45:05 +0300

    05.10.24 2ч

--- module_3/module_3_1.py
+++ module_3/module_3_1.py
@@ -1,13 +1,12 @@
 calls = 0
 
 while True:
-    """string = input("Введите слово: ")"""
-    string = 'qwer'
+    string = input("Введите слово: ")
 
     def count_calls(calls):
         calls += 1
         return calls
-
+    
     def string_info(string):
         count_calls(calls)
         tuple = []
@@ -20,24 +19,30 @@ while True:
 
     list_to_search = string_info(string)
 
-    def is_contains(string, list_to_search):
+    def is_contains(list_to_search, string):
         count_calls(calls)
         lst = {}
         for i in range(0, len(list_to_search)):
             if list_to_search[i] == string:
                 lst[list_to_search[i]] = True
             else:
-                lst[list_to_search [i]] = False
-        counter = 0
+                lst[list_to_search[i]] = False
+        calls_true = 0
         for i in lst:
-            sorting = lst.get(i)
-            if sorting == True:
-                counter +=1
-        if 1 == counter:
+            if lst[i] == True:
+                calls_true +=1
+            if lst[i] == 0:
+                counter = string.lower()
+                for i in range(0, len(list_to_search)):
+                    if list_to_search[i] == counter:
+                        calls_true +=1
+        if calls_true >= 1:
             return True
         else:
             return False
-
+    
     print(string_info(string))
-    print(is_contains(string, list_to_search))
+    print(is_contains(list_to_search, string))
+    print(calls)
+
 
