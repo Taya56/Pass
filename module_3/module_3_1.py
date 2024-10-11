c6c18ddd24b4a059fff6254f58f2f3e35bf19b60
Taya56 <krasnyanskay78@gmail.com>
2024-10-11 21:32:45 +0300

    11.10.24

--- module_3/module_3_1.py
+++ module_3/module_3_1.py
@@ -1,48 +1,30 @@
 calls = 0
 
-while True:
-    string = input("Введите слово: ")
+def count_calls(calls):
+    return calls
 
-    def count_calls(calls):
-        calls += 1
-        return calls
-    
-    def string_info(string):
-        count_calls(calls)
+def string_info(string):
+    global calls
+    calls += 1
+    tuple = (len(string), string.upper(), string.lower())
+    return tuple
+
+def is_contains(string, list_to_search):
+    global calls
+    calls += 1
+    for i in list_to_search:
         tuple = []
-        for i in range(0, 3):
-            tuple.append(i)
-        tuple[0] = len(string)
-        tuple[1] = string.upper()
-        tuple[2] = string.lower()
-        return tuple
+        tuple.append(i.lower())
+    if string.lower() in tuple:
+        return True
+    else:
+        return False
 
-    list_to_search = string_info(string)
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
 
-    def is_contains(list_to_search, string):
-        count_calls(calls)
-        lst = {}
-        for i in range(0, len(list_to_search)):
-            if list_to_search[i] == string:
-                lst[list_to_search[i]] = True
-            else:
-                lst[list_to_search[i]] = False
-        calls_true = 0
-        for i in lst:
-            if lst[i] == True:
-                calls_true +=1
-            if lst[i] == 0:
-                counter = string.lower()
-                for i in range(0, len(list_to_search)):
-                    if list_to_search[i] == counter:
-                        calls_true +=1
-        if calls_true >= 1:
-            return True
-        else:
-            return False
-    
-    print(string_info(string))
-    print(is_contains(list_to_search, string))
-    print(calls)
 
 
